3f3ca1097cb90e5927da67dacc290f0868596d52
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-04-29 13:28:47 -0300

    Realizando calculo de IMC dos pacientes com js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-var titulo = document.querySelector(".titulo");
-console.log(titulo);
-console.log(titulo.textContent);
+//AULA 1
+// var titulo = document.querySelector(".titulo");
+// console.log(titulo);
+// console.log(titulo.textContent);
 
-titulo.textContent = "Aparecida Nutricionista";
+// titulo.textContent = "Aparecida Nutricionista";
+
+//AULA 2
+var paciente = document.querySelector("#primeiro-paciente")
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+var textoImc = paciente.querySelector(".info-imc");
+
+var pesoValido = (peso > 0 && peso < 1000);
+var alturaValida = (altura > 0 && altura < 4)
+
+if(pesoValido && alturaValida)
+{
+    var imc = (peso / Math.pow(altura, 2));
+    textoImc.textContent = imc;
+}
+else
+{
+    textoImc.textContent = "Altura ou peso inválidos";
+}
